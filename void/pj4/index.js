4db186867687bd5770712f0e065d7b33d50e00b3
Thanhphong03 <thanhphongtran1703@gmail.com>
2026-09-05 20:25:50 +0300

    a bit of this a bit of that

--- void/pj4/index.js
+++ void/pj4/index.js
@@ -2,4 +2,24 @@ console.log(`hi`);
 console.log(`sup`);
 
 document.getElementById("myH1").textContent = `Hello`;
-document.getElementById("myP").textContent = `Sup`;
+
+
+let x = 100;
+
+console.log(x);
+
+//window.alert(x);
+
+let age = 23;
+
+console.log(`You are ${age} years old`);
+
+let y;
+
+console.log(typeof y);
+
+let fullName = "Phong";
+
+document.getElementById("p1").textContent = fullName;
+document.getElementById("p2").textContent = age;
+document.getElementById("p3").textContent = `You are ${age} years old`
