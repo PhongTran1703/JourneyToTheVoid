5f42e53819309d1f53559f06f3206c99e1820719
Phong Tran <thanhphongtran1703@gmail.com>
2026-09-09 03:35:26 +0000

    updated

--- void/index.js
+++
@@ -1,9 +0,0 @@
-
-const myButton = document.getElementById("myButton");
-const display = document.getElementById("display")
-
-
-myButton.onclick = function() {
-    display.textContent = Math.floor(Math.random() * 6 + 1);
-
-}
---
+++ void/pj7/index.js
@@ -0,0 +1,30 @@
+const myCheckbox = document.getElementById("myCheckbox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const paypalBtn = document.getElementById("paypalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function(){
+    if(myCheckbox.checked){
+        subResult.textContent = `You are subscribed`;
+    }
+    else{
+        subResult.textContent = `You are NOT subscribed`;
+    }
+
+    if(visaBtn.checked){
+        paymentResult.textContent = `You are paying with visa`;
+    }
+    else if(masterCardBtn.checked){
+        paymentResult.textContent = `You are paying with mastercard`;
+    }
+    else if(paypalBtn.checked){
+        paymentResult.textContent = `You are paying with PayPal`;
+    }
+    else{
+        paymentResult.textContent = `You have to select a payment method`
+    }
+}
+
